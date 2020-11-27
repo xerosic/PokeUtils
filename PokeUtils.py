3a7d6dd75c8ec3c21a3084106b4fb5e852eb2df1
XanaxOG <42121005+XanaxOG@users.noreply.github.com>
2020-11-27 16:16:03 +0100

    Fixed bug

--- PokeUtils.py
+++ PokeUtils.py
@@ -1,10 +1,19 @@
 from Type import *
 
 if __name__ == "__main__":
+    print(r"______     _        _   _ _   _ _     ")
+    print(r"| ___ \   | |      | | | | | (_) |    ")
+    print(r"| |_/ /__ | | _____| | | | |_ _| |___ ")
+    print(r"|  __/ _ \| |/ / _ \ | | | __| | / __|")
+    print(r"| | | (_) |   <  __/ |_| | |_| | \__ \\")
+    print(r"\_|  \___/|_|\_\___|\___/ \__|_|_|___/")
+    print("                   version 0.1        ")
+    print("======================================")
+
     i = input("Type of pokemon: ")
     if instance := globals().get(i, None):
         instance = instance()
-        print("\n" + instance)
+        print(instance)
     else:
         print("No such type was found. Did you mispelled it?")
     
